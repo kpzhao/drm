423ac3d82e60ff72ccead65d38b09c9c11f86e20
zhao <9709565353@qq.com>
2022-10-21 22:40:45 +0800

    update

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -4,5 +4,5 @@ set TTY1 (tty)
 [ "$TTY1" = "/dev/tty1" ] \
 && export (/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator) \
 && set -x LANG zh_CN.UTF-8 \
-&& set -x LANGUAGE zh_CN:en_US \
-&& exec sway
+&& set -x LANGUAGE zh_CN:en_US
+#&& exec sway
