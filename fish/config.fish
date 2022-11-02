955824a57019ada85b1787c12ed88ecdd102afb4
zhao <9709565353@qq.com>
2022-11-02 15:04:28 +0800

    add fzf catppuccin theme

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,9 @@ abbr -a p-off 'set -e  all_proxy http_proxy https_proxy'
 abbr -a config '/usr/bin/git --git-dir=$HOME/.drm/ --work-tree=$HOME'
 abbr -a caffine 'systemd-inhibit --what=idle sleep inf'
 end
+
+# fzf
+set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+--color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+--color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39"
