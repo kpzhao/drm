72f42f26080fa0632f84e63c674b7bdeabb25d1f
zhao <9709565353@qq.com>
2022-11-02 16:24:13 +0800

    dump fish

--- fish/completions/proxychains.fish
+++
@@ -1,3 +0,0 @@
-complete -c proxychains -s f -r -d "Configure File"
-complete -c proxychains -s q -d "Quiet Mode"
-complete -c proxychains -d "Command to run" -x -a "(__fish_complete_subcommand -f)"
