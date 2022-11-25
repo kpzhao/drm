e2b3325ac496f21cdeeb12030219c76067018974
zhao <9709565353@qq.com>
2022-11-25 15:48:13 +0800

    use gruvbox theme in nvim

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,19 +1,19 @@
 --local colorscheme = "tokyonight"
---local colorscheme = "gruvbox"
+local colorscheme = "gruvbox"
 --local colorscheme = "nord"
 
---local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
---if not status_ok then
---  return
---end
---
-local status, bufferline = pcall(require, "catppuccin")
-if not status then
-	vim.notify("没有找到 catppuccin")
-	return
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+ return
 end
 
-require("catppuccin").setup {
-  flavour = "macchiato" -- mocha, macchiato, frappe, latte
-}
-vim.api.nvim_command "colorscheme catppuccin"
+-- local status, bufferline = pcall(require, "catppuccin")
+-- if not status then
+-- 	vim.notify("没有找到 catppuccin")
+-- 	return
+-- end
+--
+-- require("catppuccin").setup {
+--   flavour = "macchiato" -- mocha, macchiato, frappe, latte
+-- }
+-- vim.api.nvim_command "colorscheme catppuccin"
